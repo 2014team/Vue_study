572a43449f110ebcbdc6e18eb95ae26be316f56a
zhuzeqing <645874679@qq.com>
2024-08-08 15:15:18 +0800

    update

---
+++ html/46单文件组件/TestSchool.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="demo">
+        <h2>学校名称：{{name}}</h2>
+        <h2>学校地址：{{address}}</h2>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name:'TestSchool',
+        data() {
+            return {
+                name: '深圳大学',
+                address: '广东省深圳市'
+            }
+        }
+    }
+
+</script>
+
+<style>
+    .demo{
+        background-color: antiquewhite;
+    }
+
+</style>
