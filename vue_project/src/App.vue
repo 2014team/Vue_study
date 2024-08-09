1668cd0799dbec3cb36b8c34917c1c518a1f225b
zhuzeqing <645874679@qq.com>
2024-08-09 11:42:21 +0800

    update

---
+++ vue_project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <TestSchool/>
+    <TestStudent/>
+  </div>
+</template>
+
+<script>
+
+import TestSchool from './components/TestSchool.vue'
+import TestStudent from './components/TestStudent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TestSchool,
+    TestStudent
+  }
+}
+</script>
+
+
---
+++ vue_project/src/components/TestStudent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>学生名称：{{name}}</h2>
+        <h2>学生年龄：{{age}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'TestStudent',
+        data() {
+            return {
+                name: '深圳大学',
+                age: 19
+            }
+        }
+    }
+
+</script>
