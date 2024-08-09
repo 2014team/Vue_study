697ddf4753bddaf55526cbdc41c8c5562107bf3e
zhuzeqing <645874679@qq.com>
2024-08-09 15:42:22 +0800

    update

---
+++ vue_project/02_src_ref属性/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+
+    <h1 v-text="msg" ref="title"></h1>
+    <button ref="btn" @click="showDOM">点我输出DOM元素</button>
+    <TestSchool ref="sch" />
+  </div>
+</template>
+
+<script>
+
+import TestSchool from './components/TestSchool.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TestSchool,
+  },
+  data(){
+    return {
+      msg:'欢迎学习Vue'
+    }
+  },
+  methods:{
+    showDOM(){
+      console.log(this.$refs.title); // 真实DOM元素
+      console.log(this.$refs.btn);// 真实DOM元素
+      console.log(this.$refs.sch);//School组件的实例对象（vc）
+    }
+  }
+}
+</script>
+
+
